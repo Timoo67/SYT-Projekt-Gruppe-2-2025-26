98083edb8fc6f2be550120d131c0aa4e1a6cafb2
josef-singer <josef.singer@htlpinkafeld.at>
2025-10-03 09:33:11 +0200

    feat: read humidity sensor

--- Gruppe-2-SYT-Projekt-2025-26/src/main.cpp
+++ Gruppe-2-SYT-Projekt-2025-26/src/main.cpp
@@ -1,18 +1,43 @@
 #include <Arduino.h>
+#include <DHT.h>
+
+#define dhtType DHT11
+#define dhtPin 12
+
+DHT sensor(dhtPin, dhtType);
 
 // put function declarations here:
-int myFunction(int, int);
+// int myFunction(int, int);
 
 void setup() {
+  Serial.begin(9600);
+  sensor.begin();
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // int result = myFunction(2, 3);
 }
 
 void loop() {
+  float relLuftfeuchtigkeit = sensor.readHumidity();
+  float tempC = sensor.readTemperature();
+
+  if(isnan(relLuftfeuchtigkeit) || isnan(tempC))  {
+    Serial.println("Fehler beim Lesen der Daten!");
+    return;
+  }
+
+  Serial.print("rel. Luftfeuchtigkeit: ");
+  Serial.print(String(relLuftfeuchtigkeit));
+  Serial.print("%");
+  Serial.print(" | ");
+  Serial.print("Temperature: ");
+  Serial.print(String(tempC, 2));
+  Serial.println("°C");
+  delay(2000);
+
   // put your main code here, to run repeatedly:
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+// int myFunction(int x, int y) {
+//   return x + y;
+// }
